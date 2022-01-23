cd82d90baf5ef92cb5118e3f12d848d62c37c95f
ahampriyanshu <ahampriyanshu@gmail.com>
2022-01-23 23:57:39 +0530

    tailwind-react-cra

---
+++ gfg-tw/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='w-full h-full'>
+      <div className='flex justify-center'>
+        <h1 className='text-3xl lg:text-5xl font-bold text-green-600 mt-12'>
+        Geeks For Geeks
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
