a3ecf3ab660b0df329149be5072a4abd01f25d71
ahampriyanshu <ahampriyanshu@gmail.com>
2022-06-05 16:14:44 +0530

    updating sitemap

--- dummy.py
+++
@@ -1,1 +0,0 @@
-print("Geeks For Geeks")
--- sitemap/server/models/model.js
+++ sitemap/server/models/model.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose"),
   projectSchema = new mongoose.Schema(
     {
       title: { type: String, unique: true },
-      description: { type: String },
     },
     { timestamps: true }
   );
 
-module.exports = mongoose.model("Project", projectSchema);
+module.exports = mongoose.model("Todo", todoSchema);
